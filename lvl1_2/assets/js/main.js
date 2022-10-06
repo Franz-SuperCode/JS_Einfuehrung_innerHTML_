6efce6f2d38156300d929f5040be072d724d78c4
Franz-SuperCode <franzsupercode@gmail.com>
2022-10-06 12:38:57 +0200

    Fertig

---
+++ lvl1_2/assets/js/main.js
@@ -0,0 +1,4 @@
+
+document.getElementById("info").innerHTML = "<h1>Hello World</h1> <h2>How are you?</h2>";
+document.getElementById("container").innerHTML = "start of the element"
+document.write("end of the element");
---
+++ lvl1_3/assets/js/main.js
@@ -0,0 +1,1 @@
+document.getElementById("gallery").innerHTML = '<figure><figcaption>Fig1</figcaption><img src="https://images.unsplash.com/photo-1665021456203-950433e2fa80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60" alt=""></figure >';
